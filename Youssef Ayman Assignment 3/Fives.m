09cd6c50808210d875f715109c37c779d8633ef6
YoussefAyman-eng <aymany509@gmail.com>
2024-07-27 11:40:12 +0300

    my assignment

---
+++ Youssef Ayman Assignment 3/Fives.m
@@ -0,0 +1,6 @@
+function [F] = Fives(rows,columns)
+%Fives this gives you a matrix of all elements equal to five
+%   imput your number of rows and columns to this
+F = ones(rows,columns)*5;
+end
+
---
+++ Youssef Ayman Assignment 3/rotleft.m
@@ -0,0 +1,7 @@
+function r = rotleft(inputArg1,inputArg2)
+%rotleft this this shifts all the elements to the first element to its left
+%   Detailed explanation goes here
+r([1;
+outputArg2 = inputArg2;
+end
+
